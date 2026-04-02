a69c5297381a12a078f737df39dc157363264b1e
harshit bhorga <102799341+Bhorga@users.noreply.github.com>
2026-04-02 05:32:05 +0530

    added middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-
+import os
 from app.routers.question import router as question_router
 
 
@@ -10,6 +11,18 @@ app = FastAPI(
     title="LLM Tools API",
     version="0.1.0",
     description="Minimal FastAPI backend for evaluation and transcription APIs.",
+)
+
+
+# Configure CORS
+origins = [origin.strip() for origin in os.getenv("FRONTEND_URL").split(",")]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 app.include_router(question_router)
